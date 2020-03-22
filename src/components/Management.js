3b1d109b5f7c5d7ab17df31892a4a9e402114edc
reshadbinharun <md.bin_harun@tufts.edu>
2020-03-22 18:38:27 -0400

    fleshed out student management view

--- src/components/Management.js
+++ src/components/Management.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Message, Card, Form, Button, TextArea, Icon, Checkbox } from 'semantic-ui-react'
+import { Grid, Message, Card, Form, Button, TextArea, Icon } from 'semantic-ui-react'
 import { makeCall } from '../apis';
 import swal from "sweetalert";
 
@@ -8,11 +8,15 @@ export default class Messaging extends React.Component {
         super();
         this.state = {
             emailString: '',
-            pendingStudents: [],
-            sendingInvite: false
+            // TODO: remove mock data later
+            pendingStudents: [{email: 'ree@gmail.com', name: 'Ree', grade: '9th'},{email: 'ree@gmail.com', name: 'Ree 2', grade: '8th'}],
+            sendingRequest: false
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleInvite = this.handleInvite.bind(this);
+        this.generateRequests = this.generateRequests.bind(this);
+        this.handleApproval = this.handleApproval.bind(this);
+        this.handleReject = this.handleReject.bind(this);
     }
 
     handleChange(e) {
@@ -24,7 +28,142 @@ export default class Messaging extends React.Component {
 
     handleInvite(e) {
         e.preventDefault();
-        // TODO: parse emails and make API call to send emails
+        const emailsArr = this.state.emailString.replace(/\s/g, '').split(',');
+        this.setState({sendingRequest: true});
+        const payload = {
+            emails: emailsArr
+        };
+        makeCall(payload, '/invite/students', 'post').then(result => {
+            if (!result || result.error) {
+                this.setState({
+                    sendingRequest: false
+                }, () => {
+                    swal({
+                        title: "Error!",
+                        text: "There was an error inviting the student, please try again.",
+                        icon: "error",
+                    });
+                });
+            } else {
+                this.setState({
+                    sendingRequest: false,
+                    emailString: ''
+                }, () => {
+                    swal({
+                        title: "Success!",
+                        text: "You've successfully invited all students!",
+                        icon: "success",
+                    });
+                });
+            }
+        });
+    }
+
+    handleApproval(email) {
+        this.setState({sendingRequest: true});
+        const payload = {
+            approved: true,
+            email: email
+        };
+        makeCall(payload, '/approve/student', 'post').then(result => {
+            if (!result || result.error) {
+                this.setState({
+                    sendingRequest: false
+                }, () => {
+                    swal({
+                        title: "Error!",
+                        text: "There was an error approving the student, please try again.",
+                        icon: "error",
+                    });
+                });
+            } else {
+                this.setState({
+                    sendingRequest: false
+                }, () => {
+                    swal({
+                        title: "Success!",
+                        text: "You've successfully approved the students!",
+                        icon: "success",
+                    });
+                });
+            }
+        });
+    }
+
+    handleReject(email) {
+        this.setState({sendingRequest: true});
+        const payload = {
+            email: email
+        };
+        makeCall(payload, '/students/delete', 'post').then(result => {
+            if (!result || result.error) {
+                this.setState({
+                    sendingRequest: false
+                }, () => {
+                    swal({
+                        title: "Error!",
+                        text: "There was an error rejecting the student, please try again.",
+                        icon: "error",
+                    }).then(() => {
+                        window.location.reload();
+                    });
+                });
+            } else {
+                this.setState({
+                    sendingRequest: false
+                }, () => {
+                    swal({
+                        title: "Success!",
+                        text: "You've successfully rejected the student!",
+                        icon: "success",
+                    }).then(() => {
+                        window.location.reload();
+                    });
+                });
+            }
+        });
+    }
+
+    generateRequests() {
+        return this.state.pendingStudents.map(student => {
+            return (
+                <Card style={{'width': '100%'}}>
+                    <Grid>
+                        <Grid.Column width={4}
+                            style={{'margin': '2px 0 2px 0'}}
+                        >
+                            <b>{student.name}</b>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div> | </div>
+                        </Grid.Column>
+                        <Grid.Column width={4}
+                            style={{'margin': '2px 0 2px 0'}}
+                        >
+                            <div> {student.grade} Grade </div>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Button
+                                disabled={this.state.sendingRequest}
+                                style={{'height':'80%', 'margin': '2px 0 2px 0'}}
+                                onClick={() => this.handleApproval(student.email)}
+                            >
+                                Approve
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Button
+                                disabled={this.state.sendingRequest}
+                                style={{'height':'80%', 'margin': '2px 0 2px 0'}}
+                                onClick={() => this.handleReject(student.email)}
+                            >
+                                Reject
+                            </Button>
+                        </Grid.Column>
+                    </Grid>
+                </Card>
+            )
+        })
     }
 
     render() {
@@ -46,7 +185,7 @@ export default class Messaging extends React.Component {
                         <Grid.Row>
                             <Grid.Column width={12}>
                                 <TextArea 
-                                    disabled={this.state.sendingInvite}
+                                    disabled={this.state.sendingRequest}
                                     style={{'margin': '10px 0 10px 0'}}
                                     placeholder='student1@gmail.com, student2@yahoo.com, ...'
                                     name="emailString"
@@ -57,8 +196,8 @@ export default class Messaging extends React.Component {
                             <Grid.Column width={4}>
                                 <Button
                                     onClick={this.handleInvite}
-                                    loading={this.state.sendingInvite}
-                                    disabled={!this.state.emailString || this.state.sendingInvite}
+                                    loading={this.state.sendingRequest}
+                                    disabled={!this.state.emailString || this.state.sendingRequest}
                                 >
                                     <Icon name="paper plane"/>
                                     Invite
@@ -68,13 +207,13 @@ export default class Messaging extends React.Component {
                         <Grid.Row>
                             <Message
                                 style={{'margin': "20px 0 10px 0"}}
-                                content={`There are pending requests`}
+                                content={this.state.pendingStudents && this.state.pendingStudents.length ? `There are pending requests` : `There are no pending requests.`}
                             />
                         </Grid.Row>
                         <Grid.Row
                             style={{'margin': '10px 0 10px 0'}}
                         >
-                            List of pending requests
+                            {this.generateRequests()}
                         </Grid.Row>
                     </Form>
                 </Grid>
